4e93fec64940f2fa340ca279b0a3ac7f0edc4031
Alberto Rocha <contact@rocha.pro>
2023-09-26 09:36:04 -0500

    Updates in how report is displayed

--- gui.py
+++ gui.py
@@ -6,12 +6,40 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from ping3 import ping
 import database as db
+from datetime import datetime
 import datetime
 import time
 import sqlite3
 
 # Call the configure_logging function to set up logging
 logger = configure_logging(log_file='ping.log', log_level=logging.DEBUG)
+
+class LoadingDialog:
+    def __init__(self, root, task_name):
+        self.root = root
+        self.root.title("Loading")
+        self.root.geometry("300x100")
+
+        # Create a label for the loading message
+        self.loading_label = tk.Label(root, text=f"{task_name}...\nPlease wait...")
+        self.loading_label.pack(pady=20)
+
+        # Create a progress bar (optional)
+        self.progress_var = tk.DoubleVar()
+        self.progress = ttk.Progressbar(root, variable=self.progress_var, mode="indeterminate")
+        self.progress.pack(fill=tk.X, padx=20)
+
+    def close(self):
+        # Close the loading dialog
+        self.root.destroy()
+
+    def start(self):
+        # Start the progress bar (optional)
+        self.progress.start()
+
+    def stop(self):
+        # Stop the progress bar (optional)
+        self.progress.stop()
 
 # Define functions related to GUI
 
@@ -66,39 +94,75 @@ def on_generate_full_report():
     """
     Generate a full ping report for all devices and display it in the GUI.
     """
+    logger.info("Starting Full Report")
     ping_count = 4
     ping_interval = 1
     bad_results = []
+    bad_count = 0
+    disabled_devices = []
+    disabled_count = 0
+
+    # Create a loading dialog
+    loading_root = tk.Tk()
+    loading_screen = LoadingDialog(loading_root, "Generating Full Report")
+    loading_screen.start()
+    loading_root.update()
 
     for value in db.fetch_devices():
-        all_results = ping_target(value["IP"], ping_count, ping_interval)
+        if value["status"] == 1:
+            disabled_devices.append(value['name'])
+            disabled_count += 1
+            continue
+        all_results = ping_target(value["ip"], ping_count, ping_interval)
         ping_results = all_results[0]
         if all_results[1]:
             bad_results.append(all_results[1][0])
+            bad_count +=1
         generate_report(ping_results, "report_full.txt")
 
+    loading_screen.stop()
+    loading_screen.close()
+
     output_text.delete(1.0, tk.END)
+    output_text.insert(tk.END, f"{datetime.datetime.now()}.\n")
     output_text.insert(tk.END, "Full report generated.\n \n")
 
     if bad_results:
-        output_text.insert(tk.END, "Pings without response: \n")
+        output_text.insert(tk.END, f"Pings without response: {bad_count}\n")
         for bad in bad_results:
             output_text.insert(tk.END, f"{bad}.\n")
+
+    if disabled_count >= 1:
+        output_text.insert(tk.END, f"\nDisabled devices: {disabled_count}\n")
+        for disabled in disabled_devices:
+            output_text.insert(tk.END, f"{disabled}.\n")
 
 def on_generate_selected_report():
     """
     Generate a ping report for the selected device and display it in the GUI.
     """
+    logger.info("Starting the Selected Device Report")
     selected_row = select_row_from_cb()
     if selected_row == None:
         return
     ping_count = 4
     ping_interval = 1
 
+    # Create a loading dialog
+    loading_root = tk.Tk()
+    loading_screen = LoadingDialog(loading_root, "Generating Single Device Report")
+    loading_screen.start()
+    loading_root.update()
+
     ping_results = ping_target(selected_row[2], ping_count, ping_interval)[0]
     generate_report(ping_results, "report_selected_device.txt")
 
+    # Close loading window
+    loading_screen.stop()
+    loading_screen.close()
+
     output_text.delete(1.0, tk.END)
+    output_text.insert(tk.END, f"{datetime.datetime.now()}.\n")
     output_text.insert(tk.END, f"Report generated for {selected_row[2]}.\n\n")
     for result in ping_results:
         output_text.insert(tk.END, f"IP: {result[1]}, Response: {result[2]}\n")
@@ -114,9 +178,15 @@ def on_combo_select(event):
     selected_name = ip_combobox.get()
     selected_row = db.find_row(selected_name)
 
+    if selected_row[3] == 1:
+        status = "Enabled"
+    if selected_row[3] == 0:
+        status = "Disabled"
+
     if selected_row:
         name_label.config(text=f"Name: {selected_row[1]}")
         ip_label.config(text=f"IP: {selected_row[2]}")
+        status_label.config(text=f"Status: {status}")
 
 def add_device():
     """
@@ -137,17 +207,26 @@ def add_device():
     ip_entry = tk.Entry(add_window)
     ip_entry.pack()
 
+    # Create a Checkbutton for the Status field
+    status_label = tk.Label(add_window, text="Status:")
+    status_label.pack()
+    status_var = tk.IntVar()  # Use IntVar to represent 1 and 0
+    status_checkbox = tk.Checkbutton(add_window, variable=status_var, onvalue=1, offvalue=0)
+    status_checkbox.pack()
+
     def save_device():
         # Get the values from the entry widgets
         device_name = name_entry.get()
         device_ip = ip_entry.get()
+        device_status = 1 if status_var.get() else 0  # Convert 1 and 0 back to True and False
 
         if not device_name or not device_ip:
             # Check if either field is empty
             tk.messagebox.showerror("Error", "Both Name and IP are required")
             return
 
-        db.add_device(device_name, device_ip)
+        db.add_device(device_name, device_ip, device_status)
+        logger.info(f"New Device: name: {device_name}, ip: {device_ip}, status: {device_status}")
 
         # Update the device list
         update_device_list()
@@ -185,17 +264,28 @@ def update_device():
     ip_entry.pack()
     ip_entry.insert(0, selected_row[2])
 
+    # Create a Checkbutton for the Status field
+    status_label = tk.Label(update_window, text="Status:")
+    status_label.pack()
+    status_var = tk.IntVar()  # Use IntVar to represent 1 and 0
+    status_checkbox = tk.Checkbutton(update_window, variable=status_var, onvalue=1, offvalue=0)
+    status_checkbox.pack()
+    status_var.set(selected_row[3] == 1)  # Set the initial value based on the current status
+
     def save_updated_device():
         # Get the updated values from the entry widgets
         updated_name = name_entry.get()
         updated_ip = ip_entry.get()
+        updated_status = status_var.get()
 
         if not updated_name or not updated_ip:
             # Check if either field is empty
             tk.messagebox.showerror("Error", "Both Name and IP are required")
             return
 
-        db.update_device(selected_row[0], updated_name, updated_ip)
+        db.update_device(selected_row[0], updated_name, updated_ip, updated_status)
+        logger.info(f"Update device from name: {selected_row[1]}, ip: {selected_row[2]}, status: {selected_row[3]}")
+        logger.info(f"Update device to name: {updated_name}, ip: {updated_ip}, status: {updated_status}")
 
         # Update the device list
         update_device_list()
@@ -223,6 +313,7 @@ def remove_device():
     if confirmation:
         # Remove the device from the database
         db.remove_device(selected_row[0])
+        logger.info(f"Removed device name: {selected_row[1]}, ip: {selected_row[2]}")
 
         # Update the device list
         update_device_list()
@@ -247,12 +338,14 @@ def show_table():
     table_viewer_window.title("SQLite Table Viewer")
 
     # Create a Treeview widget to display the table
-    tree = ttk.Treeview(table_viewer_window, columns=("Name", "IP"))
+    tree = ttk.Treeview(table_viewer_window, columns=("Name", "IP", "Status"))
     tree.column("#0", width=0)
     tree.column("#1", width=150)
     tree.column("#2", width=110)
+    tree.column("#3", width=50)
     tree.heading("#1", text="Name")
     tree.heading("#2", text="IP")
+    tree.heading("#2", text="Status")
 
     # Define a function to populate the Treeview with data from the database
     def populate_tree():
@@ -260,7 +353,7 @@ def show_table():
         for item in tree.get_children():
             tree.delete(item)
 
-        [tree.insert("", "end", values=(item['name'], item['IP'])) for item in db.fetch_devices()]
+        [tree.insert("", "end", values=(item['name'], item['ip'], item['status'])) for item in db.fetch_devices()]
 
     # Create a "Refresh" button to populate the Treeview
     refresh_button = tk.Button(table_viewer_window, text="Refresh", command=populate_tree)
@@ -286,6 +379,21 @@ def select_row_from_cb():
         return
     return selected_row
 
+def test():
+    print("test")
+    # Connect to the SQLite database
+    conn = sqlite3.connect('device_database.db')
+    cursor = conn.cursor()
+
+    # Update the values in the 'your_column' column for all rows in the 'your_table' table
+    new_value = True
+    cursor.execute("UPDATE devices SET status = ?", (new_value,))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+
 # Create the main application window
 app = tk.Tk()
 app.title("SCADA Ping Monitor")
@@ -309,6 +417,7 @@ db_menu.add_command(label="Add Device", command=add_device)
 db_menu.add_command(label="Update Device", command=update_device)
 db_menu.add_command(label="Remove Device", command=remove_device)
 db_menu.add_command(label="Show DB", command=show_table)
+db_menu.add_command(label="test", command=test)
 
 # Full Report Button
 full_report_button = tk.Button(app, text="Generate Full Report", command=on_generate_full_report)
@@ -325,6 +434,8 @@ name_label = tk.Label(app, text="Name: ")
 name_label.pack()
 ip_label = tk.Label(app, text="IP: ")
 ip_label.pack()
+status_label = tk.Label(app, text="Status: ")
+status_label.pack()
 
 # Bind the selection event to the combo box
 ip_combobox.bind("<<ComboboxSelected>>", on_combo_select)
--- main.py
+++ main.py
@@ -3,16 +3,18 @@
 import logging
 from log_config import configure_logging
 import tkinter as tk
-from gui import create_gui
+from gui import create_gui, LoadingDialog
 from database import initialize_database
 import datetime
 import time
 
 # Call the configure_logging function to set up logging
 logger = configure_logging(log_file='ping.log', log_level=logging.DEBUG)
+logger.info("Starting the application")
 
 # Initialize the database (Assuming this function sets up your database schema)
 initialize_database()
+logger.info("Initializing the database")
 
 # Create the main application window using Tkinter
 root = tk.Tk()
@@ -22,9 +24,6 @@ root.title("SCADA Ping Monitor")
 
 # Create and display the graphical user interface by calling create_gui function
 create_gui(root)
-
-# Log an informational message indicating the start of the application
-logger.info("Starting the application")
 
 # Enter the Tkinter main event loop to start the application
 root.mainloop()
